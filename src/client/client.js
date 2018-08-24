50ba0cee95bc8f879344d0e52186e243d05e52bc
BradHick <bradleyhrm@gmail.com>
2018-08-24 15:12:45 -0300

    Test name of process.env

--- src/client/client.js
+++ src/client/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = process.env.HEROKU_URI || 'http://localhost:3030'
+const url = process.env.REACT_APP_HEROKU_URI || 'http://localhost:3030'
 
 export const client = axios.create({
   baseURL: url,
